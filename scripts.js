49850a1645e48533a20848594941577ec7ca3de7
Stephen <thestephenhunt@gmail.com>
2023-10-05 21:48:32 -0500

    Space

--- scripts.js
+++ scripts.js
@@ -236,3 +236,17 @@ if ($fav) {
     setResizeEvents($favs, $fav)
   }
 }
+
+let navbar = document.getElementById("nav");
+let navPos = navbar.getBoundingClientRect().top;
+
+window.addEventListener("scroll", e => {
+    let scrollPos = window.scrollY;
+
+    if (scrollPos > navPos) {
+        navbar.classList.add('sticky');
+      }
+      else {
+        navbar.classList.remove('sticky');
+      }
+  });
